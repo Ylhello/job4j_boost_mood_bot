701df430c5563eee877c37f4166166bdb43a6c78
Yulya <ylhappy999@gmail.com>
2025-03-29 18:44:24 +1000

    Import задания “Модели данных“ на Github [505302]

---
+++ src/main/java/ru/job4j/bmb/model/MoodContent.java
@@ -0,0 +1,41 @@
+package ru.job4j.bmb.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "mb_mood_content")
+public class MoodContent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "mood_id")
+    private Mood mood;
+
+    private String text;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Mood getMood() {
+        return mood;
+    }
+
+    public void setMood(Mood mood) {
+        this.mood = mood;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ src/main/java/ru/job4j/bmb/model/MoodLog.java
@@ -0,0 +1,54 @@
+package ru.job4j.bmb.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "mb_mood_log")
+public class MoodLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "mood_id")
+    private Mood mood;
+
+    private long createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Mood getMood() {
+        return mood;
+    }
+
+    public void setMood(Mood mood) {
+        this.mood = mood;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+}
+
